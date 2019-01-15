05f07a5073a0ef431b8c61068c33fd6417563d36
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-01-15 20:37:36 +0800

    大鱼文章内容抓取

--- dayuContent/dayu_article.py
+++ dayuContent/dayu_article.py
@@ -66,7 +66,6 @@ class Dayu(object):
         #文章频道id
         channel_id = '777'
 
-        print('img_list: ', img_list)
         print('title: ', title)
         print('url: ', url)
         print('author: ', author)
